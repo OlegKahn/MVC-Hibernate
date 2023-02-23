4cb524ea38f2ab894ec30776744313e0ca774646
Oleg Kan <olegkahn@gmail.com>
2023-02-23 22:26:29 +0900

    add crudcontroller

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -2,7 +2,6 @@ package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import web.model.User;
@@ -24,32 +23,4 @@ public class HelloController {
 		return "index";
 	}
 
-
-	@GetMapping(value = "new")
-	public String writeNewUser(Model model) {
-		User user = new User();
-		model.addAttribute("user", user);
-		return "secondPage";
-	}
-
-
-	@GetMapping(value = "/saveUser")
-	public String saveUser(@ModelAttribute User user) {
-		userService.add(user);
-		return "redirect:/";
-	}
-
-
-	@GetMapping(value = "/delete")
-	public String deleteUser(@RequestParam("userId") long id) {
-		userService.delete(id);
-		return "redirect:/";
-	}
-
-
-	@GetMapping(value = "/update")
-	public String updateUser(@RequestParam("userId") long id, Model model) {
-		model.addAttribute(userService.getUser(id));
-		return "secondPage";
-	}
 }
